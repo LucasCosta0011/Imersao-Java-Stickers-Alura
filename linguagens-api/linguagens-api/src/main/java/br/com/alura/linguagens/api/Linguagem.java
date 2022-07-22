22674afb28a8f15859b4cc73ea11b9c0554129c8
lucascosta0011 <lucascostadeveloper@gmail.com>
2022-07-22 15:34:26 -0300

    commit inicial

--- linguagens-api/linguagens-api/src/main/java/br/com/alura/linguagens/api/Linguagem.java
+++ linguagens-api/linguagens-api/src/main/java/br/com/alura/linguagens/api/Linguagem.java
@@ -1,15 +1,28 @@
 package br.com.alura.linguagens.api;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "PrincipaisLinguagens")
 public class Linguagem {
-  
+  @Id
+  private String id;
   private String title;
   private String image;
   private int ranking;
+
+  public Linguagem(){
+    
+  }
   
   public Linguagem(String title, String image, int ranking) {
     this.title = title;
     this.image = image;
     this.ranking = ranking;
+  }
+
+  public String getId() {
+    return id;
   }
 
   public String getTitle() {
--- linguagens-api/linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ linguagens-api/linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -2,13 +2,16 @@ package br.com.alura.linguagens.api;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 // Anotação
 @RestController
 public class LinguagemController {
-
+/* 
   private List<Linguagem> linguagens = 
     List.of(
       new Linguagem("Java", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png", 1),
@@ -19,10 +22,22 @@ public class LinguagemController {
   public String processaLinguagemPreferida(){
     return "Oi, Java!";
   }
+*/
+  @Autowired
+  private LinguagemRepository repositorio;
+
 
   @GetMapping(value = "/Linguagens")
   public List<Linguagem> obterLinguagens(){
-    return linguagens;
+    //List<Linguagem> linguagens = 
+    return repositorio.findAll();
+  }
+
+  @PostMapping("/Linguagens")
+  // Indicar passando no corpo da requisicao
+  public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem){
+    Linguagem linguagemSalva = repositorio.save(linguagem);
+    return linguagemSalva;
   }
 
 }
